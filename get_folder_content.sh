41066ff1eeafc9bc0c55c40378e56123c6afe43a
asmogan <asmogan@gmail.com>
2012-03-09 18:13:53 +0100

    fixed a hilarious path bug xD

--- get_folder_content.sh
+++ get_folder_content.sh
@@ -3,4 +3,4 @@
 # list all the files contained in the given directory
 # replace youre Source or DestinationPath
 
-ls -R Documents/dls/ > /Users/sebastianjenke/Dropbox/SAN.txt
+ls -R /Users/sebastianjenke/Documents/dls > /Users/sebastianjenke/Dropbox/SAN.txt
